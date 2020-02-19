ed907ff989e99a87160b92506cc5d2e8ef922896
raisusmita <susmitathulung53@gmail.com>
2020-02-19 23:25:05 +0545

    Removing CORS setup from main booked_room branch

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,6 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Cors::class // <-- add this line
 
     ];
 
@@ -64,8 +63,7 @@ class Kernel extends HttpKernel
         'password.confirm' => \Illuminate\Auth\Middleware\RequirePassword::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => \App\Http\Middleware\Cors::class // <-- add this line
+        'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
 
     ];
 
