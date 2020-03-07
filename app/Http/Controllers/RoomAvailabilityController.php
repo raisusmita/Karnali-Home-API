7dd34ccea1518b794c5f06a056062d891a8d6cd1
Prakash <dahalprakash1720@gmail.com>
2020-03-07 15:49:10 +0545

    Remove unnecessary comments

--- app/Http/Controllers/RoomAvailabilityController.php
+++ app/Http/Controllers/RoomAvailabilityController.php
@@ -11,7 +11,7 @@ class RoomAvailabilityController extends Controller
     public function getAvailableRoom()
     {
         // getting unavailable data to filter available
-        
+
         $unAvailableRoom = RoomAvailability::available()->get();
         $roomIds = [];
         foreach ($unAvailableRoom as $av) {
@@ -26,43 +26,12 @@ class RoomAvailabilityController extends Controller
         return $room;
     }
 
-    // public function getAvailableRoomByDate()
-    // {
-    //     $dateValue =  request();
-    //     if ($dateValue->check_in_date < $dateValue->check_out_date) {
-    //         // $unAvailableRoom = RoomAvailability::unavailable()
-    //         //     ->where('check_out_date', '>=', $dateValue->check_in_date or 'check_in_date', '>', $dateValue->check_out_date)
-    //         //     ->get();
-    //         $unAvailableRoom = RoomAvailability::unavailable()
-    //             // ->where('check_out_date', '>=', $dateValue->check_in_date and 'check_out_date', '>', $dateValue->check_out_date)
-    //             // ->where('check_in_date', '<', $dateValue->check_out_date and 'check_out_date', '<=', $dateValue->check_out_date)
-    //             ->whereBetween('check_in_date', [$dateValue->check_in_date, $dateValue->check_out_date])
-    //             ->get();
-    //         $roomIds = [];
-    //         foreach ($unAvailableRoom as $av) {
-    //             array_push($roomIds, $av->room_id);
-    //         }
-    //         $room = Room::whereNotIn('id', $roomIds)->get();
-    //         $room->map(function ($roomCat) {
-    //             $roomCat->roomCategory->id;
-    //         });
-    //         $room = $room->groupBy('room_category_id');
-
-    //         return $room;
-    //     } else {
-    //         return 'Invalid Date';
-    //     }
-    // }
-
-
     public function getAvailableRoomByDate()
     {
         $dateValue =  request();
 
         if ($dateValue->check_in_date < $dateValue->check_out_date) {
             $unAvailableRoom = RoomAvailability::whereDate('check_in_date', '>=', $dateValue->check_in_date, 'and', 'check_in_date', '<=', $dateValue->check_out_date)
-                // ->orWhere('check_out_date', '<=', $dateValue->check_out_date, 'and', 'check_out_date', '<=', $dateValue->check_out_date)
-                // ->orWhere(('check_out_date' >= $dateValue->check_in_date and 'check_out_date' <= $dateValue->check_in_date))
                 ->orWhereDate('check_out_date', '>=', $dateValue->check_in_date, 'and', 'check_out_date', '<=', $dateValue->check_out_date)
                 ->unavailable()
                 ->get();
@@ -80,22 +49,19 @@ class RoomAvailabilityController extends Controller
         } else {
             return 'Invalid Date';
         }
-        
     }
 
     public function storeRoomAvailability()
     {
         $roomId = request();
         $test = RoomAvailability::where('room_id', $roomId->room_id)->get();
-        if( is_null($test))
-        {
+        if (is_null($test)) {
 
             return response()->json([
                 'success' => false,
                 'message' => 'The room is already taken'
             ]);
-        }
-        else{
+        } else {
             $roomAvailability = RoomAvailability::create($this->validateRequest());
             return response()->json([
                 'success' => true,
@@ -103,8 +69,6 @@ class RoomAvailabilityController extends Controller
                 'data' => $roomAvailability
             ]);
         }
-        
-
     }
 
     private function validateRequest()
