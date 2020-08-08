6d8c86ca22eaa8ae4c571327e683df5b1d78cbd0
raisusmita <susmitathulung53@gmail.com>
2020-08-08 17:14:41 +0545

    conflicts resolved

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -14,14 +14,11 @@ class CustomerController extends Controller
         if ($customer->isNotEmpty()) {
             $customer->map(function ($customer) {
                 $customer->bookings;
-            });
-        }
-
-        if ($customer->isNotEmpty()) {
                 $customer->identity_image_first = $customer->identity_image_first ? asset('storage/' . $customer->identity_image_first) : "";
                 $customer->identity_image_second = $customer->identity_image_second ? asset('storage/' . $customer->identity_image_second) : "";
             });
             return $this->jsonResponse(true, 'Lists of Customers.', $customer);
+
         } else {
             return $this->jsonResponse(false, 'Currently, there is no any Customers yet.');
         }
